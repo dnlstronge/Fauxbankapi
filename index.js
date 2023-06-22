1af35fd24547ea0257bda897e0fd1cf4032b3368
Dan Stronge <dnlstronge@gmail.com>
2023-06-22 16:40:01 +0100

    [chore] - basic set up

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const express = require("express")
 const dotenv = require("dotenv")
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Fake banking api server');
+  });
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
